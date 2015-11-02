e46ec5e8b48488aea09413835524ce15e45e6691
Anders Wallenquist <aw@leia.vertel.se>
2015-11-02 12:00:50 +0100

    website_product_category: new method on product.category, is_translated

--- website_product_category/website_product_category.py
+++ website_product_category/website_product_category.py
@@ -45,7 +45,9 @@ class website_product_category(http.Controller):
     @http.route(['/allcategory/<model("product.category"):category>', ], type='http', auth="public", website=True)
     def get_category(self, parent_id=1, **post):
         cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
-        return request.render('website_product_category.page_allcategories', {'categories': request.env['product.category'].sudo().search([('parent_id', '=', parent_id)], order='id')})
+        categories = request.env['product.category'].sudo().search([('parent_id', '=', parent_id)], order='id')
+        #categories.filtered("category.is_translated(lang)")
+        return request.render('website_product_category.page_allcategories', {'categories': categories})
 
 
 class product_category(models.Model):
@@ -56,4 +58,6 @@ class product_category(models.Model):
     website_published = fields.Boolean('Available in the website', copy=False)
     website_image = fields.Binary('Category Image')
 
+    def is_translated(self,lang):
+        return len(self.env['ir.translation'].search(['&',('name','=','product.category,website_small_description'),('lang','=',lang)])) == 1
 
