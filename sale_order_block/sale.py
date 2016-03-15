df34b35be44d916fc1ed85ec6c4d2c3f3cd4aa6e
Axel Pennsäter <pennsater@gmail.com>
2016-03-15 09:55:02 +0100

    sale_order_block: removed remote field and short description

--- sale_order_block/sale.py
+++ sale_order_block/sale.py
@@ -21,13 +21,12 @@ class sale_order(models.Model):
     _inherit = 'sale.order'
     
     website_published = fields.Boolean()
-    @api.one
-    def _website_short_description(self):
-        self.website_short_description = re.match(re.compile('(^.*\.)'),self.website_description[:25]) and re.match(re.compile('(^.*\.)'),self.website_description[:25]).group() or self.website_description[:25]
+    #~ @api.one
+    #~ def _website_short_description(self):
+        #~ self.website_short_description = re.match(re.compile('(^.*\.)'),self.website_description[:25]) and re.match(re.compile('(^.*\.)'),self.website_description[:25]).group() or self.website_description[:25]
     #~ website_short_description = fields.Text(compute="_website_short_description")
     website_description = fields.Text(string="Description",size=100,help="Use this box for describing the quotation.")
     website_subject = fields.Char(size=25)
-    website_remote = fields.Selection([('1','Helt'),('2','Delvis')],string='Remote',required=False)
     skill_ids = fields.One2many(comodel_name='sale.order.skill',inverse_name='order_id', string='Skills')
     
     language_ids = fields.Many2many(comodel_name='hr.language', string='Languages')
@@ -57,19 +56,16 @@ class website_product_category(http.Controller):
         return request.render('sale_order_block.index', {
        
         })
-    
-    @http.route(['/sale_order_block',], type='http', auth="public", website=True)
-    def index(self, **post):
-        cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
-        #~ published = request.env['sale.order'].sudo().search([('website_published','=',True)]).sorted(key=lambda r: r.partner_id.name)
-        #~ published = published.sorted(key=lambda r: r.partner_id.name)
-        return request.render('sale_order_block.index', {
-            #~ 'active_orders': published
-        })
-    
-    @http.route(['/senaste_uppdragen',], type='http', auth="public", website=True)
-    def senaste_uppdragen(self, **post):
-        cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
-        return request.render('sale_order_block.senaste_uppdragen', {
-        })
-    
+    #~ 
+    #~ @http.route(['/sale_order_block',], type='http', auth="public", website=True)
+    #~ def index(self, **post):
+        #~ cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
+        #~ return request.render('sale_order_block.index', {
+        #~ })
+    #~ 
+    #~ @http.route(['/senaste_uppdragen',], type='http', auth="public", website=True)
+    #~ def senaste_uppdragen(self, **post):
+        #~ cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
+        #~ return request.render('sale_order_block.senaste_uppdragen', {
+        #~ })
+    #~ 
