434cff6e9a2605448767ee57e25f04f52d7c7201
Anders Wallenquist <aw@leia.vertel.se>
2016-03-23 10:32:59 +0100

    website_quote_table: old templates from erp.vertel.se

--- website_quote_table/models.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from openerp import models, fields, api
-
-# class website_quote_table(models.Model):
-#     _name = 'website_quote_table.website_quote_table'
-
-#     name = fields.Char()
