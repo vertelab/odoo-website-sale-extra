059f78e637490a3b821d057c5b59ea96f53358c9
HaojunZou <apollo_zhj@msn.com>
2016-04-05 17:30:06 +0200

    website_penta: new module added

---
+++ website_penta/penta.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution, third party addon
+#    Copyright (C) 2004-2015 Vertel AB (<http://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import openerp
+import logging
+from openerp import models, fields, api, _
+from openerp.exceptions import Warning
+from openerp import http
+from openerp.http import request
+_logger = logging.getLogger(__name__)
+
+class penta(http.Controller):
+
+    @http.route(['/find_consultant'], type='http', auth='public', website=True)
+    def find_consultant(self, **post):
+        return request.website.render('website_penta.find_consultant', {})
+
+    @http.route(['/work_with_us'], type='http', auth='public', website=True)
+    def work_with_us(self, **post):
+        return request.website.render('website_penta.work_with_us', {})
+
+    #~ @http.route(['/assign_interest'], type='http', auth='public', website=True)
+    #~ def assign_interest(self, **post):
+        #~ return request.website.render('website_penta.assign_interest', {})
+
+    #~ @http.route(['/add_assign'], type='http', auth='public', website=True)
+    #~ def add_assign(self, **post):
+        #~ return request.website.render('website_penta.add_assign', {})
+
+
+class contactus(openerp.addons.website_crm.controllers.main.contactus):
+    def create_lead(self, request, values, kwargs):
+        path_or_xml_id_or_id = "website_crm." + values['categ_ids']
+
+        #~ raise Warning(path_or_xml_id_or_id)
+
+        action_id = request.registry['ir.model.data'].xmlid_to_res_id(request.cr, request.uid, path_or_xml_id_or_id, raise_if_not_found=False)
+        #~ Add hidden specific TAG to Lead, depending on form.
+        values['categ_ids'] = [(6,0,[action_id])]
+        return super(contactus,self).create_lead(request, values, kwargs)
+
