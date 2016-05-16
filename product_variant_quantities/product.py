ecc0f02be6a6a3d7326e0e8119468b1370b45c25
Robin Chatfield <robin.chatfield@gmail.com>
2016-05-16 09:01:54 +0200

    product_variant_quantities: module created.

---
+++ product_variant_quantities/product.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# OpenERP, Open Source Management Solution, third party addon
+# Copyright (C) 2004-2015 Vertel AB (<http://vertel.se>).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields, api, _
+import openerp.addons.decimal_precision as dp
+import logging
+_logger = logging.getLogger(__name__)
+
+class product_product(models.Model):
+    _inherit = 'product.product'
+    
+    volume = fields.Float('Volume', digits_compute=dp.get_precision('Stock Volume'), help="The volume in m3.")
+    weight = fields.Float('Gross Weight', digits_compute=dp.get_precision('Stock Weight'), help="The gross weight in Kg.")
+    weight_net = fields.Float('Net Weight', digits_compute=dp.get_precision('Stock Weight'), help="The net weight in Kg.")
+        
