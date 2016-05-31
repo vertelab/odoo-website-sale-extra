4d073427a3b510f0695c662984e3b08fa39f210e
Haojun Zou <apollo_zhj@msn.com>
2016-05-31 15:47:27 +0200

    sales_cycle: added campaign_id to sale cycle

--- sales_cycle/sales_cycle.py
+++ sales_cycle/sales_cycle.py
@@ -26,32 +26,40 @@ _logger = logging.getLogger(__name__)
 class sales_cycle(models.Model):
     _name = 'sale.cycle'
     _description = 'Sales Cycle'
-    
+
     name = fields.Char('Name', required=True)
     date_start = fields.Date('Start Date', required=True)
     date_end = fields.Date('End Date', required=True)
-    
+    campaign_id = fields.Many2one(comodel_name='marketing.campaign', string='Campaign')
+
     @api.model
     def _get_last_cycle_start_date(self):
         last_cycle = self.search_read([], ['date_start'], order='date_end', limit=1)
         return last_cycle and last_cycle[0]['date_start'] or ''
-    
+
     @api.model
     def _get_last_cycle_stop_date(self):
         last_cycle = self.search_read([], ['date_end'], order='date_end', limit=1)
         return last_cycle and last_cycle[0]['date_end'] or ''
-        
+
+
+class marketing_campaign(models.Model):
+    _inherit='marketing.campaign'
+
+    sales_cycle_ids = fields.One2many(comodel_name='sale.cycle', inverse_name='campaign_id', string='Sale cycles')
+
+
 class res_partner(models.Model):
     _inherit = 'res.partner'
-    
+
     visited_last_cycle = fields.Boolean('Visited Last Cycle', compute='_compute_visited_last_cycle', search='_search_visited_last_cycle')
-    
+
     @api.one
     @api.depends('last_meeting')
     def _compute_visited_last_cycle(self):
         last_date = self.env['sale.cycle']._get_last_cycle_start_date()
         self.visited_last_cycle = last_date == self.last_meeting
-    
+
     def _search_visited_last_cycle(self, operator, value):
         start_date = self.env['sale.cycle']._get_last_cycle_start_date()
         stop_date = self.env['sale.cycle']._get_last_cycle_stop_date()
@@ -60,4 +68,4 @@ class res_partner(models.Model):
             return [('id', 'not in', [p['id'] for p in partners])]
         else:
             return ['&', ('last_meeting', '>=', start_date), ('last_meeting', '<=', stop_date)]
-            
+
