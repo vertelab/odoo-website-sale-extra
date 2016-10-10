0cdcebb1e2e0f38b2fa0f48df55f8113e8d86fab
Robin Chatfield <robin.chatfield@gmail.com>
2016-10-10 16:43:38 +0200

    sale_journal_mail: Module created.

---
+++ sale_journal_mail/sale_journal.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# OpenERP, Open Source Management Solution, third party addon
+# Copyright (C) 2004-2016 Vertel AB (<http://vertel.se>).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import api, models, fields, _
+import logging
+_logger = logging.getLogger(__name__)
+
+class sale_journal_invoice_type(models.Model):
+    _inherit = 'sale_journal.invoice.type'
+    
+    send_email = fields.Boolean('Send Email')
+
+class res_partner(models.Model):
+    _inherit ='res.partner'
+    
+    invoice_type_id = fields.Many2one('sale_journal.invoice.type', 'Invoice Type', inverse='_set_invoice_type_id')
+    
+    @api.one
+    def _set_invoice_type_id(self):
+        for partner in self.child_ids:
+            partner.invoice_type_id = self.invoice_type_id
+
+class sale_order(models.Model):
+    _inherit = 'sale.order'
+    
+    #Field for hiding/showing email buttons
+    sjm_send_email = fields.Boolean('Send Email', compute='_get_sjm_send_email')
+    
+    @api.one
+    @api.depends('invoice_type_id', 'invoice_type_id.send_email')
+    def _get_sjm_send_email(self):
+        if self.invoice_type_id and self.invoice_type_id.send_email:
+            self.sjm_send_email = True
+        else:
+            self.sjm_send_email = False
+    
+    @api.multi
+    def onchange_partner_id(self, part):
+        res = super(sale_order, self).onchange_partner_id(part)
+        if part:
+            partner = self.env['res.partner'].search_read([('id', '=', part)], ['invoice_type_id'])
+            invoice_type = partner[0].get('invoice_type_id')
+            res['value']['invoice_type_id'] = invoice_type and invoice_type[0]
+        return res
+    
+    @api.model
+    def create(self, values):
+        if not values.get('invoice_type_id') and values.get('partner_id'):
+            partner = self.env['res.partner'].search_read([('id', '=', values.get('partner_id'))], ['invoice_type_id'])
+            if partner:
+                invoice_type = partner[0].get('invoice_type_id')
+                values['invoice_type_id'] = invoice_type and invoice_type[0]
+        return super(sale_order, self).create(values)
