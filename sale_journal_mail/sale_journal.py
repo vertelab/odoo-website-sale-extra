f93a71bcb68a9b457cdee40a1d02be8f34c77692
Robin Chatfield <robin.chatfield@gmail.com>
2016-10-19 16:55:49 +0200

    sale_journal_mail: Fixed duplicate and unnecesary fields.

--- sale_journal_mail/sale_journal.py
+++ sale_journal_mail/sale_journal.py
@@ -28,20 +28,9 @@ class sale_journal_invoice_type(models.Model):
     
     send_email = fields.Boolean('Send Invoice by Email')
 
-class res_partner(models.Model):
-    _inherit ='res.partner'
-    
-    invoice_type_id = fields.Many2one('sale_journal.invoice.type', 'Invoice Type', inverse='_set_invoice_type_id')
-    
-    @api.one
-    def _set_invoice_type_id(self):
-        for partner in self.child_ids:
-            partner.invoice_type_id = self.invoice_type_id
-
 class account_invoice(models.Model):
     _inherit = 'account.invoice'
     
-    invoice_type_id = fields.Many2one('sale_journal.invoice.type', 'Invoice Type', compute='_get_invoice_type_id')
     #Field for hiding/showing email buttons
     sjm_send_email = fields.Boolean('Send Email', compute='_get_invoice_type_id')
     
@@ -49,31 +38,10 @@ class account_invoice(models.Model):
     @api.depends('order_id', 'picking_id')
     def _get_invoice_type_id(self):
         if self.order_id:
-            self.invoice_type_id = self.order_id.invoice_type_id
+            invoice_type_id = self.order_id.invoice_type_id
         elif self.picking_id:
-            self.invoice_type_id = self.picking_id.invoice_type_id
-        if self.invoice_type_id and self.invoice_type_id.send_email:
+            invoice_type_id = self.picking_id.invoice_type_id
+        if invoice_type_id and invoice_type_id.send_email:
             self.sjm_send_email = True
         else:
             self.sjm_send_email = False
-
-class sale_order(models.Model):
-    _inherit = 'sale.order'
-    
-    @api.multi
-    def onchange_partner_id(self, part):
-        res = super(sale_order, self).onchange_partner_id(part)
-        if part:
-            partner = self.env['res.partner'].search_read([('id', '=', part)], ['invoice_type_id'])
-            invoice_type = partner[0].get('invoice_type_id')
-            res['value']['invoice_type_id'] = invoice_type and invoice_type[0]
-        return res
-    
-    @api.model
-    def create(self, values):
-        if not values.get('invoice_type_id') and values.get('partner_id'):
-            partner = self.env['res.partner'].search_read([('id', '=', values.get('partner_id'))], ['invoice_type_id'])
-            if partner:
-                invoice_type = partner[0].get('invoice_type_id')
-                values['invoice_type_id'] = invoice_type and invoice_type[0]
-        return super(sale_order, self).create(values)
