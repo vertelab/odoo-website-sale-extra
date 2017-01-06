2861a374910cb0e4836e606d3d4f3833f62adfb7
Haojun Zou <apollo_zhj@msn.com>
2017-01-06 20:21:40 +0100

    website_product_snippet: updated method 1, using each function for every product_snippet

--- website_product_snippet/static/src/js/product_snippet.js
+++ website_product_snippet/static/src/js/product_snippet.js
@@ -4,18 +4,17 @@ function product_change(product){
         openerp.jsonRpc("/product_snippet/product_change", "call", {
             'product': product
         }).done(function(data){
-            $(".product_snippet").find("#ps_img").attr({
-                "id": "ps_img_product_" + data['id'],
-                "data-cke-saved-src": "data:image/png;base64," + data['image'],
-                "src": "data:image/png;base64," + data['image'],
-            });
-            $(".product_snippet").find("#ps_name").html("<span>" + data['name'] + "</span>");
-            $(".product_snippet").find("#ps_name").attr({
-                "id": "ps_name_product_" + data['id'],
-            });
-            $(".product_snippet").find("#ps_desc").html("<span>" + data['description'] + "</span>");
-            $(".product_snippet").find("#ps_desc").attr({
-                "id": "ps_desc_product_" + data['id'],
+            $.each($(".product_snippet"), function() {
+                var src = $(this).find(".img").attr("data-cke-saved-src");
+                if(src != undefined && !src.match("^data:")) {
+                    $(this).find(".img").attr({
+                        "data-cke-saved-src": "data:image/png;base64," + data['image'],
+                        "src": "data:image/png;base64," + data['image']
+                    });
+                    $(this).find("h2").html("<span>" + data['name'] + "</span>");
+                    $(this).find("p").html("<span>" + data['description'] + "</span>");
+                    return false;
+                }
             });
         });
     }
